1961169659e68dc1728125deb425b5b311e658db
Sakthibats <baskaran.sakthivel@gmail.com>
2022-10-03 17:08:24 +0800

    changed name

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <nav className="navbar navbar-dark bg-primary" style={{"minHeight":"90px"}}>
         <div className="container-fluid">
-            <span className="navbar-brand mb-0 h1">GovTech Football Tournament</span>
+            <span className="navbar-brand mb-0 h1">Football Tournament</span>
         </div>
     </nav>
   )
